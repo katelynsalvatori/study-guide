95b49280889757d70d298bbef72165412a8d5c3e
Katelyn Salvatori <katelyn.salvatori@gmail.com>
2017-10-29 19:15:07 -0400

    Implemented study guide selection and creation

--- study_guide.py
+++ study_guide.py
@@ -9,6 +9,50 @@ except:
 
 cursor = connection.cursor()
 
+
+def could_not_access_db():
+    print "Could not access the database"
+    quit()
+
+def get_users():
+    try:
+        cursor.execute("""SELECT * FROM users;""")
+    except:
+        could_not_access_db()
+
+    return cursor.fetchall()
+
+
+def add_user_to_db(name):
+    try:
+        cursor.execute("""INSERT INTO users(name) VALUES('%s');""" % name)
+        print "User created successfully:", name
+        connection.commit()
+    except:
+        print "Could not create user - make sure your username is unique"
+
+
+def add_study_guide_to_db(name, owner_id):
+    try:
+        cursor.execute("""INSERT INTO study_guides(name, owner_id) VALUES('%s', '%s');
+        SELECT currval('study_guides_id_seq');""" % (name, owner_id))
+        print "Study guide created successfully:", name
+        connection.commit()
+    except:
+        print "Could not create study guide"
+
+    return cursor.fetchone()[0]
+
+
+def get_study_guides_for_user(owner_id):
+    try:
+        cursor.execute("""SELECT id, name FROM study_guides WHERE owner_id = '%s';""" % owner_id)
+    except:
+        could_not_access_db()
+
+    return cursor.fetchall()
+
+
 # ******** MENUS ********
 def select_or_create_user_menu():
     selection = ""
@@ -28,7 +72,7 @@ def select_or_create_study_guide(user_id):
     selection = ""
 
     while selection != "1" and selection != "2":
-        selection = raw_input("1. Select exisiting study guide\n2. Create a new study guide\nSelect an option:")
+        selection = raw_input("1. Select exisiting study guide\n2. Create a new study guide\nSelect an option: ")
 
     if selection is "1":
         select_study_guide(user_id)
@@ -37,25 +81,9 @@ def select_or_create_study_guide(user_id):
 
 
 # ******** USER MANAGEMENT ********
-def get_users():
-    try:
-        cursor.execute("""SELECT * FROM users;""")
-    except:
-        print "Could not access the database"
-        quit()
-
-    return cursor.fetchall()
-
-
 def create_user():
     name = raw_input("Enter name of user: ")
-    try:
-        cursor.execute("""INSERT INTO users(name) VALUES('%s');""" % name)
-        print "User created successfully:", name
-        connection.commit()
-    except:
-        print "Could not create user - make sure your username is unique"
-
+    add_user_to_db(name)
     select_or_create_user_menu()
 
 
@@ -64,7 +92,7 @@ def select_user():
     users = get_users()
     ids = []
     for (id, name) in users:
-        print("""%s: %s""" % (id, name))
+        print("%s: %s" % (id, name))
         ids.append(id)
 
     while int(selection) not in ids:
@@ -75,11 +103,34 @@ def select_user():
 
 # ******** STUDY GUIDE MANAGEMENT ********
 def select_study_guide(user_id):
-    return
+    selection = "0"
+    study_guides = get_study_guides_for_user(user_id)
+    ids = []
+
+    if len(study_guides) == 0:
+        print "No study guides for selected user! Please create a study guide."
+        select_or_create_study_guide(user_id)
+
+    for (id, name) in study_guides:
+        print("%s: %s" % (id, name))
+        ids.append(id)
+
+    while int(selection) not in ids:
+        selection = raw_input("Enter a study guide number: ")
+
+    begin_studying(int(selection))
 
 
 def create_study_guide(user_id):
+    name = raw_input("Enter name of study guide: ").replace("'", "''")
+    study_guide_id = add_study_guide_to_db(name, user_id)
+    select_or_create_study_guide(user_id)
+
+
+# ******** STUDYING ********
+def begin_studying(study_guide_id):
     return
+
 
 # ******** MAIN ********
 if __name__ == '__main__':
